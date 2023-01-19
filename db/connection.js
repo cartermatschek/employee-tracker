c4085420345cefa48aa06bf6c51f065245c873c4
Carter Matschek <carter.matschek@gmail.com>
2023-01-19 10:10:26 -0600

    added .env

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2')
+require ('dotenv').config()
 
 const db = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
-      port: 3306,
-      password: 'Beltre!1986',
-      database: 'employees_db'
+      port: process.env.PORT,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME
     },
     console.log(`Connected to the employees_db database.`)
   );
